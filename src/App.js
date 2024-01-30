02a52a1ce1a5e31246ea3aebe7c24927e50c2c09
Your Name <you@example.com>
2024-01-30 18:08:50 +0300

    İlk commit: weatherApp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { apiKey } from './weather';
+import { turkishCities } from './turkeyCities';
+import "./App.css"
 
-function App() {
+const App = () => {
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=tr&units=metric&appid=${apiKey}`);
+        setWeatherData(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    const formattedCity = turkishCities.find(c => c.toLowerCase() === city.toLowerCase());
+    if (formattedCity) {
+      setCity(formattedCity);
+      fetchData();
+    } else {
+      setWeatherData(null);
+    }
+  }, [city]);
+
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  const filterFiveDaysData = () => {
+    // Sadece 5 günü al
+    const filteredData = weatherData.list.filter((item, index) => index % 8 === 0);
+    return filteredData;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1 className='tittle'>Günlük Hava Durumu</h1>
+      <input
+        className='citySerch'
+        type="text"
+        placeholder="Şehir Giriniz.."
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      />
+      {weatherData ? (
+        <div className='container'>
+          <h2>{capitalizeFirstLetter(weatherData.city.name)}</h2>
+          <div className="daily-weather-container">
+            {filterFiveDaysData().map((item, index) => (
+              <div key={index} className="daily-weather">
+                <p>{item.dt_txt.split(' ')[0]}</p>
+                <p>Sıcaklık: {item.main.temp} °C</p>
+                <p>Hava: {capitalizeFirstLetter(item.weather[0].description)}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div className='contain'>
+          <h2>Hava Durumu Bilgisini Alın!</h2>
+          <p>Şehir adınızı girerek 5 günlük hava durumu bilgisini öğrenin.</p>
+        </div>
+      )}
+
     </div>
   );
 }
---
+++ src/weather.js
@@ -0,0 +1,1 @@
+export const apiKey = "39a97614b78c90f68bac716652356fcc" ;
